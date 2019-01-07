ae54f1cbefe951a24e6a1581a0c36dd6032c4ebb
Amarnath TSR <tsr.amarnath@gmail.com>
2019-01-07 22:35:08 +0530

    Connected to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const uri = require("./config/keys").mongoUri;
 
 const app = express();
 
 const port = process.env.port || 5000;
+
+mongoose
+  .connect(
+    uri,
+    {
+      useNewUrlParser: true
+    }
+  )
+  .then(() => console.log("DB connected success"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
   res.status(200).send({ message: "success" });
