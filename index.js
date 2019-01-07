c14440f16ed80beff15c085bcdde02bc1a305924
Amarnath TSR <tsr.amarnath@gmail.com>
2019-01-07 22:38:18 +0530

    enable body parser for app

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const bodyparser = require("body-parser");
+
 const uri = require("./config/keys").mongoUri;
 
 const app = express();
@@ -16,6 +18,26 @@ mongoose
   )
   .then(() => console.log("DB connected success"))
   .catch(err => console.log(err));
+
+app.use(
+  bodyparser.urlencoded({
+    extended: false
+  })
+);
+
+// parse application/json
+app.use(bodyparser.json());
+
+// access control origin server
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.status(200).send({ message: "success" });
