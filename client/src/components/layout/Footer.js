662173dd32986ce4905e39e0e14648d0461a2748
Amarnath TSR <tsr.amarnath@gmail.com>
2019-01-08 12:00:48 +0530

    footer component created

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="page-footer">
+        <div className="footer-copyright">
+          <div className="container">
+            © 2018 Copyright Author
+            <a className="grey-text text-lighten-4" href="#!">
+              Amarnath TSR
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
