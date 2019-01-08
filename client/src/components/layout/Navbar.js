97349ca8266c43d9b458d751a8f23294d65ab92d
Amarnath TSR <tsr.amarnath@gmail.com>
2019-01-08 15:44:38 +0530

    Added guest link and auth link for login and guest users

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,28 +1,73 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
 
 class Navbar extends Component {
+  onLogoutClick(e) {
+    e.preventDefault();
+    this.props.logoutUser();
+    // Redirect to login
+    window.location.href = "/login";
+  }
+
   render() {
+    const { isAuthenticated, isAdmin } = this.props.auth;
+
+    const guestLinks = (
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    );
+    const authLinks = (
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <a href="" onClick={this.onLogoutClick.bind(this)}>
+            Logout
+          </a>
+        </li>
+      </ul>
+    );
+
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="/" className="brand-logo">
             Interview
           </a>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li>
-              <a href="/contact">Contact Us</a>
-            </li>
-            <li>
-              <a href="/register">Register</a>
-            </li>
-            <li>
-              <a href="/login">Login</a>
-            </li>
-          </ul>
+          {isAuthenticated ? authLinks : guestLinks}
         </div>
       </nav>
     );
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Navbar);
