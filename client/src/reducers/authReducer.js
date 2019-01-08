f489d2cb65f62680ae0f8278d8dcadc491c377a9
Amarnath TSR <tsr.amarnath@gmail.com>
2019-01-08 18:43:45 +0530

    Get all users in auth reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,27 +1,34 @@
-import { SET_CURRENT_USER } from "../actions/types";
+import { SET_CURRENT_USER, GET_USERS, USERS_LOADING } from "../actions/types";
 import isEmpty from "../validations/is-empty";
 
 const initialState = {
- isAuthenticated : false,
- isAdmin:false,
- user: {}
+  isAuthenticated: false,
+  user: {},
+  users: null
 };
 
-export default function(state = initialState, action){
-    switch(action.type){
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
 
-        case SET_CURRENT_USER:
-        console.log(action.payload)
-        return{
-            ...state,
-            isAuthenticated: !isEmpty(action.payload),
-            isAdmin: (action.payload.role === "admin"),
-            user: action.payload
-           
-        }
+    case GET_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false
+      };
+    case USERS_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
 
-       
-        default: 
-        return state;
-    }
+    default:
+      return state;
+  }
 }
