261609909b5561e986b01f08c6e580c82c076f31
Amarnath TSR <tsr.amarnath@gmail.com>
2019-01-08 08:46:13 +0530

    validation included and enable for post contact router

--- routes/api/email.js
+++ routes/api/email.js
@@ -2,12 +2,18 @@
 const express = require("express");
 const router = express.Router();
 
+// import validation
+
+const userEmailInput = require("../../validation/email");
+
 // post to contact
 
-router.post('contact',(req,res)=>{
+router.post("contact", (req, res) => {
+  const { errors, isValid } = userEmailInput(req.body);
 
-})
-
-
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+});
 
 module.exports = router;
