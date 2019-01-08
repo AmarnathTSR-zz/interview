b76aceedd22034c7e9ec6fa5ce13ad54677174b8
Amarnath TSR <tsr.amarnath@gmail.com>
2019-01-08 15:10:17 +0530

    dashboard route included

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import Dashboard from "./components/dashboard/Dashboard";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -45,6 +46,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/login" component={Login} />
               <Route exact path="/register" component={Register} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
