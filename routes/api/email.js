2a2a19f28e4a0719caf91eeff01998dd3a252c58
Amarnath TSR <tsr.amarnath@gmail.com>
2019-01-08 08:35:35 +0530

    email router created for contact form

---
+++ routes/api/email.js
@@ -0,0 +1,13 @@
+// include third party module
+const express = require("express");
+const router = express.Router();
+
+// post to contact
+
+router.post('contact',(req,res)=>{
+
+})
+
+
+
+module.exports = router;
